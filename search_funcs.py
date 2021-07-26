315d2010fa4c456dd3ff52e6de53e194be2ca715
Raleigh Barden <rbardenksu@gmail.com>
2021-07-26 00:34:06 -0400

    reorganize files

--- search_funcs.py
+++ search_funcs.py
@@ -1,96 +1,41 @@
-#%%
-import copy
-from itertools import product
+# %%
+from search_funcs import partition, get_paths_cost, get_paths
 
-#%%
-def partition(collection):
-    """Basic Partition function.
-    This was used from https://stackoverflow.com/a/30134039/13419030
-    Since partitioning was not the main focus of the project, and this seemed to
-    be a common implementation, I figured there was no issue using this function"""
-    global counter
-    if len(collection) == 1:
-        yield [collection]
-        return
-    first = collection[0]
-    for smaller in partition(collection[1:]):
-        for n, subset in enumerate(smaller):
-            yield smaller[:n] + [[first] + subset] + smaller[(n + 1) :]  # noqa: E203
-        yield [[first]] + smaller
-
-
-#%%
-def get_corpus(map, in_chosen_nodes):
-    """Function used to create a corpus of partitions already used.
-    This is used to calculate the cost of nodes"""
-    local_corpus = set()
-    for level_ind, node_ind in enumerate(in_chosen_nodes):
-        local_corpus |= set([tuple(val) for val in map[level_ind][node_ind]])
-    return local_corpus
-
-
-#%%
-def get_node_cost(in_node, corpus):
-    """Used to get the cost of a node, the cost of the node is reduced if it
-    is made up of grams that have been selected already"""
-    node = in_node[:]
-    for gram_ind, gram in enumerate(node):
-        if tuple(gram) in corpus:
-            node[gram_ind] = 0
-        else:
-            node[gram_ind] = 1
-
-    return sum(node)
-
-
-#%%
-def get_indices(map):
-    """Used to get the indices of the nodes in the map"""
-    indices = []
-    for level in map:
-        level_indices = []
-        for node_ind, _ in enumerate(level):
-            level_indices.append(node_ind)
-        indices.append(level_indices)
-    return indices
-
-
-#%%
-def get_seq_cost(map, seq):
-    """Returns the cost of a sequence where in enumerated form,
-    The index is the index in 'levels' of a sentance's partitions,
-    the value is the partition set selected"""
-    seq_vals = copy.deepcopy(seq)
-    seq_costs = []
-    for lev_ind, nod_ind in enumerate(seq_vals):
-        seq_costs.append(
-            get_node_cost(
-                map[lev_ind][nod_ind],
-                get_corpus(map=map, in_chosen_nodes=tuple(seq_vals[:lev_ind])),
-            )
-        )
-    return seq_costs
-
-
-#%%
-def get_paths(map, prepend_paths=[]):
-    """Function used to get all possible paths from the map"""
-    if len(prepend_paths) > 0:
-        for pre_path in prepend_paths:
-            for path in product(*get_indices(map)):
-                yield pre_path + path
-    else:
-        for path in product(*get_indices(map)):
-            yield path
-
-
-#%%
-def get_paths_cost(map, paths):
-    """Function used to get the cost of each path,
-    the cost is the sum of the cost of each node"""
-    for path in paths:
-        cost = get_seq_cost(map, path)
-        yield (path, cost, sum(cost))
+# %%
+def k_best_paths(part_ds, start_k=0, k=2, buffer=1, prepend_paths=[]):
+    """Returns the best paths in the first k entries in the given partitioned dataset"""
+    tree = {}
+    lowest_cost = float("inf")
+    for path, node_cost, tot_cost in get_paths_cost(
+        map=part_ds, paths=get_paths(part_ds[start_k:k], prepend_paths)
+    ):
+        tree[path] = {"node_cost": node_cost, "tot_cost": tot_cost}
+        if tot_cost < lowest_cost:
+            lowest_cost = tot_cost
+    filtered_dict = {k: v for k, v in tree.items() if v["tot_cost"] <= lowest_cost}
+    return filtered_dict
 
 
 # %%
+def find_best_paths(map, incr=3, buffer=1):
+    """Finds the best paths in the given map"""
+    best_paths = [()]
+    start_ind = 0
+    far_ind = incr
+    while far_ind <= len(map):
+        best_paths = list(
+            k_best_paths(
+                map,
+                start_k=start_ind,
+                k=far_ind,
+                buffer=buffer,
+                prepend_paths=best_paths,
+            ).keys()
+        )
+        print(best_paths, start_ind, far_ind)
+        start_ind = far_ind
+        if far_ind < len(map) and far_ind + incr >= len(map):
+            far_ind = len(map)
+        else:
+            far_ind += incr
+    return best_paths
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # %%
-from search_funcs import partition, get_paths_cost, get_paths
+from search_funcs import find_best_paths
+from helper_funcs import partition
 import pandas as pd
 
 # %%
@@ -21,46 +22,6 @@ news_partitioned = partition_dataset(news_dataset)
 
 ABC_dataset = ["A B", "A C", "B A", "B C", "C A", "C B", "A C"]
 ABC_partitioned = partition_dataset(ABC_dataset)
-
-# %%
-def k_best_paths(part_ds, start_k=0, k=2, buffer=1, prepend_paths=[]):
-    """Returns the best paths in the first k entries in the given partitioned dataset"""
-    tree = {}
-    lowest_cost = float("inf")
-    for path, node_cost, tot_cost in get_paths_cost(
-        map=part_ds, paths=get_paths(part_ds[start_k:k], prepend_paths)
-    ):
-        tree[path] = {"node_cost": node_cost, "tot_cost": tot_cost}
-        if tot_cost < lowest_cost:
-            lowest_cost = tot_cost
-    filtered_dict = {k: v for k, v in tree.items() if v["tot_cost"] <= lowest_cost}
-    return filtered_dict
-
-
-# %%
-def find_best_paths(map, incr=3, buffer=1):
-    """Finds the best paths in the given map"""
-    best_paths = [()]
-    start_ind = 0
-    far_ind = incr
-    while far_ind <= len(map):
-        best_paths = list(
-            k_best_paths(
-                map,
-                start_k=start_ind,
-                k=far_ind,
-                buffer=buffer,
-                prepend_paths=best_paths,
-            ).keys()
-        )
-        print(best_paths, start_ind, far_ind)
-        start_ind = far_ind
-        if far_ind < len(map) and far_ind + incr >= len(map):
-            far_ind = len(map)
-        else:
-            far_ind += incr
-    return best_paths
-
 
 # %%
 find_best_paths(ABC_partitioned, incr=3, buffer=3)
